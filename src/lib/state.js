23e5caf31af73bf357ec4140f8b3ebce7230a933
AlyxMoon <allisterkmoon@gmail.com>
2021-09-01 11:21:32 -0700

    add some other functions for creating units with default values

--- src/lib/state.js
+++ src/lib/state.js
@@ -1,11 +1,16 @@
-export const createNewHero = () => ({
-  health: 100,
-  attack: 1,
-  defense: 1,
+export const createNewUnit = (name, health, attack, defense) => ({
+  name: name,
+  health: health,
+  attack: attack,
+  defense: defense,
 })
 
-export const createNewEnemy = () => ({
-  health: 10,
-  attack: 1,
-  defense: 1,
+export const createNewHero = () => createNewUnit('hero', 100, 1, 1)
+export const createNewRat = () => createNewUnit('rat', 10, 1, 1)
+export const createNewSlime = () => createNewUnit('slime', 15, 2, 3)
+export const createNewSkeleton = () => createNewUnit('skeleton', 25, 5, 5)
+
+export const updateUnitState = (unit, property, newValue) => ({
+  ...unit,
+  [property]: unit[property] + newValue,
 })
--- tests/unit/state.test.js
+++ tests/unit/state.test.js
@@ -1,19 +1,50 @@
 import * as gameState from '@/lib/state'
 
 describe('state functionality', () => {
-  describe('createNewHero()', () => {
-    it('creates a new hero, duh!', () => {
-      const unit = gameState.createNewHero()
+  describe('unit creation stuff', () => {
+    it('createNewUnit() does that', () => {
+      expect(gameState.createNewUnit('hi', 1, 1, 1)).toEqual({
+        name: 'hi', health: 1, attack: 1, defense: 1,
+      })
 
-      expect(unit).toEqual({ health: 100, attack: 1, defense: 1 })
+      expect(gameState.createNewUnit('player', 10, 2, 3)).toEqual({
+        name: 'player', health: 10, attack: 2, defense: 3,
+      })
+    })
+
+    it('createNewHero() correctly sets values', () => {
+      expect(gameState.createNewHero()).toEqual({
+        name: 'hero', health: 100, attack: 1, defense: 1,
+      })
+    })
+
+    it('createNewRat() correctly sets values', () => {
+      expect(gameState.createNewRat()).toEqual({
+        name: 'rat', health: 10, attack: 1, defense: 1,
+      })
+    })
+
+    it('createNewSlime() correctly sets values', () => {
+      expect(gameState.createNewSlime()).toEqual({
+        name: 'slime', health: 15, attack: 2, defense: 3,
+      })
+    })
+
+    it('createNewSkeleton() correctly sets values', () => {
+      expect(gameState.createNewSkeleton()).toEqual({
+        name: 'skeleton', health: 25, attack: 5, defense: 5,
+      })
     })
   })
 
-  describe('createNewEnemy()', () => {
-    it('creates a new enemy, duh!', () => {
-      const unit = gameState.createNewEnemy()
+  describe('updateUnitState()', () => {
+    it('will update the state for a property for a unit', () => {
+      const unit = gameState.createNewUnit('', 100, 1, 1)
+      const updated = gameState.updateUnitState(unit, 'attack', 2)
 
-      expect(unit).toEqual({ health: 10, attack: 1, defense: 1 })
+      expect(updated).toEqual({
+        name: '', health: 100, attack: 3, defense: 1,
+      })
     })
   })
 })
